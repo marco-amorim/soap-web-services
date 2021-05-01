7249a33dbeb2b4e48a4365d9774a69dfc53e9941
marco-amorim <marcoamorim95@hotmail.com>
2021-05-01 13:23:19 -0300

    setting up the project

---
+++ src/test/java/com/soap/webservices/coursemanager/CourseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soap.webservices.coursemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

07f51ee733f46fc5a7c2d3debc58d865c5d85bb5
marco-amorim <marcoamorim95@hotmail.com>
2021-05-05 18:34:20 -0300

    refactor project to java 8 to generate classes from xsd

---
+++ src/main/java/com/in28minutes/courses/GetAllCourseDetailsResponse.java
@@ -0,0 +1,71 @@
+
+package com.in28minutes.courses;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="CourseDetails" type="{http://in28minutes.com/courses}CourseDetails" maxOccurs="unbounded"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "courseDetails"
+})
+@XmlRootElement(name = "GetAllCourseDetailsResponse", namespace = "http://in28minutes.com/courses")
+public class GetAllCourseDetailsResponse {
+
+    @XmlElement(name = "CourseDetails", namespace = "http://in28minutes.com/courses", required = true)
+    protected List<CourseDetails> courseDetails;
+
+    /**
+     * Gets the value of the courseDetails property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the courseDetails property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getCourseDetails().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link CourseDetails }
+     * 
+     * 
+     */
+    public List<CourseDetails> getCourseDetails() {
+        if (courseDetails == null) {
+            courseDetails = new ArrayList<CourseDetails>();
+        }
+        return this.courseDetails;
+    }
+
+}

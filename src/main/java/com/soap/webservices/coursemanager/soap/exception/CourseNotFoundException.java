d268ffb99e034212bf71343ab4adeb7716ea6eb8
marco-amorim <marcoamorim95@hotmail.com>
2021-05-08 22:15:34 -0300

    example of custom fault code in SOAP

--- src/main/java/com/soap/webservices/coursemanager/soap/exception/CourseNotFoundException.java
+++ src/main/java/com/soap/webservices/coursemanager/soap/exception/CourseNotFoundException.java
@@ -3,7 +3,7 @@ package com.soap.webservices.coursemanager.soap.exception;
 import org.springframework.ws.soap.server.endpoint.annotation.FaultCode;
 import org.springframework.ws.soap.server.endpoint.annotation.SoapFault;
 
-@SoapFault(faultCode = FaultCode.CLIENT)
+@SoapFault(faultCode = FaultCode.CUSTOM, customFaultCode = "{http://in28minutes.com/courses}001_COURSE_NOT_FOUND")
 public class CourseNotFoundException extends RuntimeException {
     public CourseNotFoundException(String message) {
         super(message);
